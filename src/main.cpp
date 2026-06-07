208de449076cc3a743ab14274a768f881bc8be83
Mercurio1509 <125401207+Mercurio1509@users.noreply.github.com>
2026-06-07 13:15:55 -0600

    Initial CPU support (#7)
    * Initial commit for CPU part
    
    * Update memory address
    
    * changes
    
    * Mock CPU to fix compiling errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <systemc>
 #include "cpu/cpu.h"
+#include <tlm_utils/simple_initiator_socket.h>
 #include "bus/bus.h"
 #include "ram/ram.h"
 #include "disk/disk.h"
 #include "accelerator/accelerator.h"
 
+struct DummyInitiator : sc_core::sc_module {
+    tlm_utils::simple_initiator_socket<DummyInitiator> init_socket;
+
+    DummyInitiator(sc_core::sc_module_name name)
+        : sc_module(name)
+        , init_socket("init_socket") {
+    }
+};
+
 int sc_main(int argc, char* argv[]) {
-    CPU         cpu("cpu");
+    DummyInitiator dummy("dummy");
+    // CPU         cpu("cpu");
     Bus         bus("bus");
     RAM         ram("ram");
     Disk        disk("disk");
     Accelerator accelerator("accelerator");
 
-    cpu.init_socket.bind(bus.target_socket);
+    dummy.init_socket.bind(bus.target_socket);
+    // cpu.init_socket.bind(bus.target_socket);
     accelerator.init_socket.bind(bus.target_socket_accel);
 
     bus.init_socket_ram.bind(ram.target_socket);
