18d10fd1d6501677dfc3a86bac0ed1b93a4af460
Abstractize <gababarcaa@gmail.com>
2026-06-15 22:48:51 -0600

    Refactor Disk module: update address constants and include memory map

--- src/disk/disk.h
+++ src/disk/disk.h
@@ -3,6 +3,7 @@
 #include <systemc>
 #include <tlm>
 #include <tlm_utils/simple_target_socket.h>
+#include "../utils/memory_map.h"
 
 SC_MODULE(Disk) {
 public:
@@ -12,13 +13,10 @@ public:
 
 private:
     void b_transport(tlm::tlm_generic_payload& payload, sc_core::sc_time& delay);
-  
-    // Rutas a las carpetas de entrada y salida
+
     static constexpr const char* INPUT_PATH  = "images/input/imagen.raw";
     static constexpr const char* OUTPUT_PATH = "images/output/salida.raw";
- 
-    // Offsets que el CPU usa para distinguir lectura y escritura
-    static constexpr uint64_t DISK_INPUT_ADDR  = 0x00000000ULL;
-    static constexpr uint64_t DISK_OUTPUT_ADDR = 0x01000000ULL;
 
+    static constexpr uint64_t DISK_INPUT_ADDR  = DISK_BASE + DISK_IMG_IN;
+    static constexpr uint64_t DISK_OUTPUT_ADDR = DISK_BASE + DISK_IMG_OUT;
 };
